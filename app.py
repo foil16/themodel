af82df24ae8d840e6d8143ec9538c946e08aa053
NA29 <nathan.audegond@hotmail.com>
2024-01-20 22:20:52 -0500

    themodel commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import CORS
 import os
 from werkzeug.utils import secure_filename
@@ -11,7 +11,7 @@ UPLOAD_FOLDER = 'uploaded_images'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['uploaded_images'] = UPLOAD_FOLDER
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
@@ -20,14 +20,16 @@ def process_image():
         return 'No file part', 400
 
     file = request.files['frame']
+    print("\n\n\n")
+    print(type(file))
     print(file)
     filename = secure_filename(f'{uuid.uuid4()}.png')
-    save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    print(filename)
+    save_path = os.path.join(app.config['uploaded_images'], filename)
     file.save(save_path)
     response = make_response(f'File saved to {save_path}', 200)
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
     
-
 if __name__ == '__main__':
     app.run(debug=True)
